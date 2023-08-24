688cd58d15eafc3fa9fefb6d68dba440146d32a5
D-Sint <m_sint@i.ua>
2023-08-24 13:54:14 +0300

    setting for YCM

--- after_install_OS.sh
+++ after_install_OS.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
+git clone https://github.com/D-Sint/Settings/after_install_OS.sh ~/
 
-sudo apt install tree htop neovim vim-python-jedi ripgrep conky-all -y
+echo "Install neccessary packages"
+sudo apt install tree htop vim vim-python-jedi ripgrep conky-all -y
 
 sudo apt install git zsh -y
 
@@ -16,10 +18,22 @@ fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # change shell to zsh
+echo "change shell on zsh"
 chsh -s $(which zsh)
 
 # change language to UA
+echo "setup language UA"
 if locale -a | grep uk_UA;then
   export LANG=uk_UA.utf-8
 fi
 
+echo "setup vim/vundle [don't miss setup YCM/reload it]"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "please install plugins INSIDE VIM. After this install clang-completer for YCM\
+  and restart it."
+vim
+
+echo "Install YCM --clang-completer"
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --clang-completer
+vim
