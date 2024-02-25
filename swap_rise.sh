a8f17c1938719f10f6d348dc7180eb66a465fad1
D-Sint <m_sint@i.ua>
2024-02-25 23:16:56 +0200

    rise swap 2 to 8 Gb

--- swap_rise.sh
+++ swap_rise.sh
@@ -1,4 +1,19 @@
 # Вимкнути існуючий своп
+<<<<<<< HEAD
+sudo swapoff /swapfile
+
+# Розширити файл свопу до 8 гігабайт
+sudo dd if=/dev/zero of=/swapfile bs=1G count=6 seek=2
+
+# Відформатувати розширений файл як своп
+sudo mkswap /swapfile
+
+# Увімкнути новий своп
+sudo swapon /swapfile
+
+# Перевірити, що своп успішно збільшено
+free -h
+=======
 sudo swapoff /mnt/swapfile
 
 # Розширити файл свопу до 8 гігабайт
@@ -12,3 +27,4 @@ sudo swapon /mnt/swapfile
 
 # Перевірити, що своп успішно збільшено
 free -h
+>>>>>>> 0f002ca480f2a3c0e4b5eb262997cf65472a4a89
